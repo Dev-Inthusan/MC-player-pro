393904a0e671ddd15ee449d0bfc6b347b3b166b4
Cristiano <in2cr7@gmail.com>
2024-01-31 04:24:34 +0530

    MC-player

---
+++ mc.js
@@ -0,0 +1,96 @@
+
+
+let allMusic = [
+    {
+      name: "Dusk till dawn",
+      artist: "Zayn Malik",
+      img: "/assets/zayndtd.jpg",
+      src: "/assets/Dusktill.webm"
+    },
+    {
+      name: "Unstopable",
+      artist: "Sia",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "No Lie Lie",
+      artist: "Dua lipoa",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Hope",
+      artist: "XXX Tentacian",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "I'm So Sorry",
+      artist: "Justin Bieber",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "love me like u do",
+      artist: "Elle",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Paradise",
+      artist: "Coolio",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Dreamers",
+      artist: "Youngook",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Diamonds in the Sky",
+      artist: "Rihanna",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Broken Angel",
+      artist: "xxx",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Let me down Slowly",
+      artist: "yyyy",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Dreamon",
+      artist: "javel",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Maroon Animal",
+      artist: "Maroon",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Enemy",
+      artist: "imagin Dragons",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
